fc1e20d9b6efa039535309f7540c9efffbf90012
Alexandra Bunch <alexandrabunch@icloud.com>
2024-07-28 10:17:57 -0400

    Fixing canvas app attempt

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ var libraryScreenOpen = document.querySelector("#libraryopen");
 libraryScreenClose.addEventListener("click", () => closeWindow(libraryScreen));
 libraryScreenOpen.addEventListener("click", () => openWindow(libraryScreen));
 
-var canvasScreen = document.querySelector("#canvas");
+var canvasScreen = document.querySelector("#canvas_app");
 var canvasScreenClose = document.querySelector("#canvasclose");
 var canvasScreenOpen = document.querySelector("#canvasopen");
 
@@ -108,4 +108,67 @@ function handleWindowTap(element) {
     biggestIndex++; 
     element.style.zIndex = biggestIndex;
     topBar.style.zIndex = biggestIndex + 1;
+}
+
+
+
+
+// When true, moving the mouse draws on the canvas
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvas.addEventListener('mousedown', (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+canvas.addEventListener('mouseup', (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = document.getElementById('selColor').value;
+  context.lineWidth = document.getElementById('selWidth').value;
+  context.lineJoin = "round";
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.closePath();
+  context.stroke();
+}
+
+function clearArea() {
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+function updateLineWidth() {
+  context.lineWidth = document.getElementById("selWidth").value;
+  // Use 'width' as needed, e.g., update canvas drawing context
+}
+
+function updateColor() {
+  vcontext.strokeStyle = document.getElementById("selColor").value;
+  // Use 'color' as needed, e.g., update canvas drawing context
 }
